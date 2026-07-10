7735593dec9c249e6fec1544dad038a572034d3e
Kim Joshua Noyad <k.noyad.535584@umindanao.edu.ph>
2026-07-10 12:52:43 +0800

    feat: add controllers and routes

---
+++ src/routes/pokemonRoutes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import * as pokemonController from '../controllers/pokemonController.js';
+
+const router = Router();
+
+router.get('/', pokemonController.getHomePage);
+router.get('/search', pokemonController.searchPokemon);
+router.get('/type/:type', pokemonController.getPokemonByType);
+router.get('/pokemon/:nameOrId', pokemonController.getPokemonDetails);
+
+router.get('/api/pokemon', pokemonController.apiGetAllPokemon);
+router.get('/api/pokemon/search', pokemonController.apiSearchPokemon);
+router.get('/api/pokemon/:nameOrId', pokemonController.apiGetPokemonDetails);
+router.get('/api/types', pokemonController.apiGetTypes);
+router.get('/api/types/:type', pokemonController.apiGetPokemonByType);
+
+export default router;
